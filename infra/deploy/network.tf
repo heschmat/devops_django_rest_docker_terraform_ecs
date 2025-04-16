5b76f1d6fb62d15a9badccd28a74db0ef6fa941d
Hesch <heschmat.seyedi@gmail.com>
2025-04-16 11:30:45 +0200

    Add public & private subnets.

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -21,3 +21,91 @@ resource "aws_internet_gateway" "main" {
     Name = "${local.prefix}-main"
   }
 }
+
+###########################################################
+# Public Subnet for public access to load balancer        #
+###########################################################
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+}
+
+resource "aws_route_table" "publi_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.publi_a.id
+}
+
+resource "aws_route" "public_internet_access_a" {
+  route_table_id         = aws_route_table.publi_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# 2nd public subnet -----------
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+}
+
+resource "aws_route_table" "publi_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.publi_b.id
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id         = aws_route_table.publi_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+############################################################
+# private subnets for internal access only                 #
+############################################################
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-private-b"
+  }
+}
+
