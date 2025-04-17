35d8a97a76084fab1b5ec6d54f611f29811979df
Hesch <heschmat.seyedi@gmail.com>
2025-04-17 19:05:06 +0200

    Add ecs permissions, cluster, task exec role, task role & log group.

---
+++ infra/deploy/ecs.tf
@@ -0,0 +1,50 @@
+###########################################
+# ECS cluster for running app on Fargate. #
+###########################################
+
+# ----
+# this gives Fargate permissions to start a new task.
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to logs."
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-exec-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
+
+# ssm access ---
+resource "aws_iam_policy" "task_ssm_policy" {
+  name        = "${local.prefix}-task-ssm-role-policy"
+  path        = "/"
+  description = "Allow Systems Manager (SSM) to execute in container"
+  policy      = file("./templates/ecs/task-ssm-policy.json")
+}
+
+resource "aws_iam_role" "app_task" {
+  name               = "${local.prefix}-app-task"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
+  role       = aws_iam_role.app_task.name
+  policy_arn = aws_iam_policy.task_ssm_policy.arn
+}
+
+# log group -----
+resource "aws_cloudwatch_log_group" "ecs_task_logs" {
+  name = "${local.prefix}-api"
+}
+
+# cluster -------
+resource "aws_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
+}
